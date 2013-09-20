4393cbf319d16ac56c696814ff412fb377d084d5
Haley Young <haleyyoung88@gmail.com>
2013-09-20 15:10:56 -0700

    Turned skeleton right side up and smoothed out skeleton tracking.

--- kinectTutorial/MainWindow.xaml.cs
+++ kinectTutorial/MainWindow.xaml.cs
@@ -32,12 +32,22 @@ namespace kinectTutorial
         void StartKinectST()
         {
             kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
-            kinect.SkeletonStream.Enable(); // Enable skeletal tracking
+
+            var parameters = new TransformSmoothParameters
+            {
+                Smoothing = 0.3f,
+                Correction = 0.0f,
+                Prediction = 0.0f,
+                JitterRadius = 1.0f,
+                MaxDeviationRadius = 0.5f
+            };
+
+            kinect.SkeletonStream.Enable(parameters); // Enable skeletal tracking
 
             skeletonData = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
 
             kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(SensorSkeletonFrameReady); // Get Ready for Skeleton Ready Events
-
+            
             kinect.Start(); // Start Kinect sensor
         }
 
@@ -65,7 +75,6 @@ namespace kinectTutorial
         {
             if (skeleton != null)
             {
-                System.Console.WriteLine("skeleton is not null");
                 // Obtain the left elbow joint; if tracked, print its position
                 Joint leftElbow = skeleton.Joints[JointType.ElbowLeft];
                 Joint rightElbow = skeleton.Joints[JointType.ElbowRight];
@@ -75,17 +84,9 @@ namespace kinectTutorial
                 double rangeHigh = leftElbow.Position.Y + 0.1;
 
                 if (head.TrackingState == JointTrackingState.Tracked)
-                {
-                   ///SetEllipsePosition(head);
-                    System.Console.WriteLine(head.Position.X);
-                    System.Console.WriteLine(head.Position.Y);
-                    
+                {                    
                     EllipseCanvas.Children.Clear();
 
-                    //Ellipse follow = new Ellipse() { Height = 100, Width = 100, Fill = Brushes.Red };
-                    //EllipseCanvas.Children.Add(follow);
-                    //Canvas.SetTop(follow, head.Position.Y * 250 + 250);
-                    //Canvas.SetLeft(follow, head.Position.X * 250 + 250);
                     foreach (Joint joint in skeleton.Joints)
                     {
                        DrawJoint(joint);
@@ -128,10 +129,10 @@ namespace kinectTutorial
         {
             if (joint.TrackingState == JointTrackingState.Tracked)
             {
-                Ellipse follow = new Ellipse() {Height = 25, Width = 25, Fill = Brushes.MediumOrchid};
+                Ellipse follow = new Ellipse() {Height = 5, Width = 5, Fill = Brushes.MediumOrchid};
                 EllipseCanvas.Children.Add(follow);
-                Canvas.SetTop(follow, joint.Position.Y * 250 + 250);
-                Canvas.SetLeft(follow, joint.Position.X * 250 + 250);
+                Canvas.SetTop(follow, joint.Position.Y * -250 + 247.5);
+                Canvas.SetLeft(follow, joint.Position.X * 250 + 247.5);
             }
         }
 
@@ -139,55 +140,12 @@ namespace kinectTutorial
         {
             if (start.TrackingState == JointTrackingState.Tracked && end.TrackingState == JointTrackingState.Tracked)
             {
-                Point p1 = new Point(start.Position.X * 250 + 250, start.Position.Y * 250 + 250);
-                Point p2 = new Point(end.Position.X * 250 + 250, end.Position.Y * 250 + 250);
+                Point p1 = new Point(start.Position.X * 250 + 250, start.Position.Y * -250 + 250);
+                Point p2 = new Point(end.Position.X * 250 + 250, end.Position.Y * -250 + 250);
                 Line line = new Line() {X1 = p1.X, Y1 = p1.Y, X2 = p2.X, Y2 = p2.Y, Stroke = Brushes.Red, StrokeThickness = 3};
                 EllipseCanvas.Children.Add(line);
             }
         }
-       
-        /*private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            System.Console.Out.Write("loaded");
-            if (KinectSensor.KinectSensors.Count > 0)
-            {
-                _sensor = KinectSensor.KinectSensors[0];
-                _tracker = new Tracker(_sensor);
-
-                Console.Read();
-
-                if (_sensor.Status == KinectStatus.Connected)
-                {
-                    _sensor.ColorStream.Enable();
-                    _sensor.DepthStream.Enable();
-                    _sensor.SkeletonStream.Enable();
-                    _sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(_sensor_AllFramesReady);
-                    try
-                    {
-                        _sensor.Start();
-                    }
-                    catch (System.IO.IOException)
-                    {
-                        Message.Text = "Something is wrong, try unplugging and plugging your kinect back in.";
-                        Message.Background = new SolidColorBrush(Colors.Azure);
-                        Message.Foreground = new SolidColorBrush(Colors.BlanchedAlmond);
-                    }
-                }
-                else if (_sensor.Status == KinectStatus.Disconnected)
-                {
-                    Message.Text = "Please connect your kinect sensor.";
-                    Message.Background = new SolidColorBrush(Colors.Azure);
-                    Message.Foreground = new SolidColorBrush(Colors.BlanchedAlmond);
-                }
-                else if (_sensor.Status == KinectStatus.NotPowered)
-                {
-                    Message.Text = "Please plug in your kinect to a power source.";
-                    Message.Background = new SolidColorBrush(Colors.Azure);
-                    Message.Foreground = new SolidColorBrush(Colors.BlanchedAlmond);
-                }
-            }
-        }*/
-
 
         private void TrackClosestSkeleton()
         {
@@ -216,7 +174,8 @@ namespace kinectTutorial
                 }
             }
         }
-        // this event fires wehn Color/Depth/Skeleton are syncronized
+
+        // this event fires when Color/Depth/Skeleton are syncronized
         void _sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
             using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
@@ -245,122 +204,14 @@ namespace kinectTutorial
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs c)
         {
-            //StopKinect(_sensor);
             Application.Current.Shutdown();
         }
-
 
         private void MouseMoveHandler(object sender, MouseEventArgs e)
         {
             System.Windows.Point position = e.GetPosition(this);
             double pX = position.X;
             double pY = position.Y;
-
-
-          // Canvas.SetLeft(headEllipse, pX);
-           //Canvas.SetTop(headEllipse, pY);
         }
-
-
     }
 }
-
-
-    // @Author - John Elsbree (Microsoft Corporation)
-    /*public class Tracker : MainWindow
-    {
-        private Skeleton[] skeletons = null;
-        
-
-        public Tracker()
-        {
-            kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
-            kinect.SkeletonStream.Enable();
-            // Connect the skeleton frame handler and enable skeleton tracking
-            //sensor.SkeletonFrameReady += SensorSkeletonFrameReady;
-            //sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(SensorSkeletonFrameReady);
-            //sensor.SkeletonStream.Enable();
-
-            kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(SensorSkeletonFrameReady); // Get Ready for Skeleton Ready Events
-
-            kinect.Start(); // Start Kinect sensor
-        }
-
-        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
-        {
-            // Access the skeleton frame
-            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
-            {
-                if (skeletonFrame != null)
-                {
-                    if (this.skeletons == null)
-                    {
-                        // Allocate array of skeletons
-                        this.skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
-                    }
-
-                    // Copy skeletons from this frame
-                    skeletonFrame.CopySkeletonDataTo(this.skeletons);
-
-                    // Find first tracked skeleton, if any
-                    Skeleton skeleton = this.skeletons.Where(s => s.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
-
-                    if (skeleton != null)
-                    {
-                        // Obtain the left elbow joint; if tracked, print its position
-                        Joint leftElbow = skeleton.Joints[JointType.ElbowLeft];
-                        Joint rightElbow = skeleton.Joints[JointType.ElbowRight];
-                        Joint head = skeleton.Joints[JointType.Head];
-
-                        double rangeLow = leftElbow.Position.Y - 0.1;
-                        double rangeHigh = leftElbow.Position.Y + 0.1;
-
-                        if (head.TrackingState == JointTrackingState.Tracked)
-                        {
-                            SetEllipsePosition(head);
-
-                            if (rightElbow.Position.Y < leftElbow.Position.Y)
-                            {
-                               // Console.WriteLine("Keep your elbows at the same height");
-                                //Console.WriteLine("Left elbow: " + leftElbow.Position.X +
-                                //    ", " + leftElbow.Position.Y + ", " + leftElbow.Position.Z);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        public void SetEllipsePosition(Joint joint)
-        {
-
-
-            Console.WriteLine("what are we looking at? " + headEllipse.Name + " left " + Canvas.GetLeft(headEllipse) + " top " + Canvas.GetTop(headEllipse));
-            Console.WriteLine("x position " + (joint.Position.X * 250 + 250));
-
-            Canvas.SetLeft(headEllipse, joint.Position.X * 250 + 250);
-            Canvas.SetTop(headEllipse, joint.Position.Y * 250 + 250);
-            EllipseCanvas.Children.Clear();
-            Console.WriteLine("now we are we looking a? " + headEllipse.Name + " left " + Canvas.GetLeft(headEllipse) + " top " + Canvas.GetTop(headEllipse));
-
-       /*     Dispatcher.Invoke((Action)(() =>
-            {
-                            System.Console.WriteLine("in dispatcher");
-            Ellipse follow = new Ellipse() { Height = 100, Width = 100, Fill = Brushes.Red };
-
-
-            double pX = joint.Position.X;
-            double pY = joint.Position.Y;
-
-
-
-            Canvas.SetLeft(follow, (pX * 100) + 50);
-            Canvas.SetTop(follow, (pY * 100) + 50);
-
-            EllipseCanvas.Children.Add(follow);
-            EllipseCanvas.InvalidateVisual();
-                
-            }));*/
-        //}
-    // End @Author - John Elsbree
-    //}
-//}
